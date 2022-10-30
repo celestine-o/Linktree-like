2b40968740156dad347b676d2bcb4f0f9a0ebc4a
Celestine <macstine@gmail.com>
2022-10-31 00:56:03 +0100

    Created a component Social
    Created a component Social to house the social media icons which when clicked on takes you to the social media page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react"
 import Footer from "./components/Footer"
+import Social from "./components/Social"
 
 function App() {
 
   return (
     <div className="App">
+      <Social />
       <Footer />
     </div>
   )
